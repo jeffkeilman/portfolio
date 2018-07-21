1d6f4916881a28cb6a1d8520f704e42422a062a7
Jeff Keilman <jkeilman93@gmail.com>
2018-07-21 18:54:16 -0400

    Hang construction sign

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>This site is under construction!</h3>
+        <h4>Interested in chatting? Feel free to email me at <a href="mailto:jkeilman93@gmail.com" target="_top">jkeilman93@gmail.com</a>.</h4>
+        <h4>I look forward to chatting with you soon.</h4>
       </div>
     );
   }
