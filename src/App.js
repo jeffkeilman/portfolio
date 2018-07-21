271bdfec47462f4367acd62af807e1cbd7e8cb0c
Jeff Keilman <jkeilman93@gmail.com>
2018-07-21 19:01:31 -0400

    Chage wording

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <h3>This site is under construction!</h3>
         <h4>Interested in chatting? Feel free to email me at <a href="mailto:jkeilman93@gmail.com" target="_top">jkeilman93@gmail.com</a>.</h4>
-        <h4>I look forward to chatting with you soon.</h4>
+        <h4>I look forward to speaking with you soon.</h4>
       </div>
     );
   }
