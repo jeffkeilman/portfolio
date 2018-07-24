0210c02c81ff2b283a4a66432f2cce3fd8b82108
Jeff Keilman <jkeilman93@gmail.com>
2018-07-24 00:25:40 -0400

    Add BS, background, delete package-lock

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h3>This site is under construction!</h3>
-        <h4>Interested in chatting? Feel free to email me at <a href="mailto:jkeilman93@gmail.com" target="_top">jkeilman93@gmail.com</a>.</h4>
-        <h4>I look forward to speaking with you soon.</h4>
+      <div className="App container">
+        <div className="row">
+          <p className="name">Jeff Keilman</p>
+        </div>
       </div>
     );
   }
