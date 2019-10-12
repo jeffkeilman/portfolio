e1339a5eb4facd76027010014b0ff03c0b50b89d
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-10-12 18:07:01 -0400

    Initial redesign

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Card from "./Card.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <div className="row">
-          <p className="name">Jeff Keilman</p>
-        </div>
+      <div className="App">
+        <Card />
       </div>
     );
   }
